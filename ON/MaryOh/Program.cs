b778670891581f54b1c371cae0ead27d51d17074
Mary Oh <mary.christine.oh@gmail.com>
2020-04-07 13:59:02 -0700

    Initializing HelloWorld Project

---
+++ ON/MaryOh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaryOh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
