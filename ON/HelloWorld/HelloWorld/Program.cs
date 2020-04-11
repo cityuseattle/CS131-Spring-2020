73e44d434e10c45a3237bc899624a1af3ad73af4
Ron Odey <ronodey83@outlook.com>
2020-04-11 16:39:36 -0500

    HOP01 - Data Types & Variables Challenge

---
+++ ON/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Name = "Ronald";
+            int Age = 37;
+            bool isMale = true;
+            double MyFloat = 10.1;
+
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(isMale);
+            Console.WriteLine(MyFloat);
+           
+
+        }
+    }
+}
