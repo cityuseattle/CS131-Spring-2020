8577bcec989607dc7437ea849277dc4dea2523b1
David Yi <yidavidh@outlook.com>
2020-04-14 17:07:44 -0700

    Module1Coding

---
+++ ON/Module1/Program1.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string Name = "David";
+           int Age = 25;
+           bool isFemale = false;
+           double MyFloat = 10.1;
+
+           Console.WriteLine(Name);
+           Console.WriteLine(Age);
+           Console.WriteLine(isFemale);
+           Console.WriteLine(MyFloat);
+        }
+    }
+}
