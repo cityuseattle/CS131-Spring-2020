8d464bb9f87385a6575e4718ab6e6d35fe775876
hollyminch <63515777+hollyminch@users.noreply.github.com>
2020-04-17 16:33:16 -0700

    Submit Hands on 1

---
+++ ON/Holly-Minch/Module1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                string Name = "Holly";
+                int Age = 37;
+                bool isFemale = true;
+
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(isFemale);
+        }
+    }Random
+}
