0535dc459eb46dc84b3092988ef839eafd811b0d
Marshall Townsend <Marshall@cityuniversity.edu>
2020-04-20 10:56:17 -0700

    Ta da

---
+++ ON/Module 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+            numbers[3] = 4;
+            numbers[4] = 5;
+
+            Console.WriteLine(numbers[1]);
+
+        }
+    }
+}
