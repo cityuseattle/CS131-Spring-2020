f81187b92077936cdb30b2daa0d927e185a3e011
Juwan Flowers <juwanflowers@Juwans-MacBook-Pro.local>
2020-04-20 17:00:45 -0700

    another try

---
+++ ON/Practice1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");//another change
+        }
+    }
+}
