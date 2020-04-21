1836414e88e78845b9343aa03af9d0b4eeddcdd3
David Yi <yidavidh@outlook.com>
2020-04-21 13:00:34 -0700

    PE2 Exercise 1

---
+++ ON/Module2/PE2/Types/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //set variables
+            int score = 77;
+            string total = "Value 100";
+            float num = 22.25f;
+            int numbers = (int)num; //casting float to int
+            
+            //write variables out
+            Console.WriteLine(score);
+            Console.WriteLine(total);
+            Console.WriteLine(num);
+            Console.WriteLine(numbers);
+        }
+    }
+}
