dd5e740e071e49308dfa572a30c7b3835c409920
Jeremy <jkgrub@gmail.com>
2020-04-25 12:41:29 -0700

    Module 3

---
+++ ON/JeremyGrub/Module3/Operators-Routines/Increment-Decrement/Increment-Decrement/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Increment_Decrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int a = 15; a >= 1; a--)         //a starts at 0, then is set to <=10, then is looped in increments of 1 up to 10
+            {
+                Console.WriteLine(a);         
+            }
+        }
+    }
+}
