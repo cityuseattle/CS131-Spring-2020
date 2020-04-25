bde974ac1a3770b2b85f97c8f5a8cf51e0c6e5ef
Priyanka Kamdar <pkamdar27@gmail.com>
2020-04-25 14:18:11 -0700

    HOP03 - Challenge 2 and 3

---
+++ ON/PriyankaKamdar/Module3/PE03/Randomly/Randomly/RandomlyProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Randomly
+{
+    class RandomlyProgram
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int a = rnd.Next(1, 100);
+            int b = rnd.Next(1, 100);
+
+            Console.WriteLine("Value of a: {0}", a);
+            Console.WriteLine("Value of b: {0}", b);
+            Console.WriteLine("Sum: {0} + {1} = {2}", a, b, a + b);
+            Console.WriteLine("Subtract: {0} - {1} = {2}", a, b, a - b);
+            Console.WriteLine("Multiply: {0} * {1} = {2}", a, b, a * b);
+            Console.WriteLine("Divide: {0} / {1} = {2}", a, b, a / b);
+            Console.WriteLine("Remainder: {0} % {1} = {2}", a, b, a % b);          
+        }
+    }
+}
---
+++ ON/PriyankaKamdar/Module3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Increment_Decrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a <= 10; a++)  // Value of a is assigned with a condition of a <= 10 to get the numbers less than or equals to 10. 
+                                          //  Also increment expresion a++ is used to loop the variable.
+            {
+                Console.WriteLine(a);
+            }
+
+
+        }
+    }
+}
