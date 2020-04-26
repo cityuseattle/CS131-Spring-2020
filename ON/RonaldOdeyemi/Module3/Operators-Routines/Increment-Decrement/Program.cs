7e23c6e4b1c256c6fcfc236adb0b050b25ca05dd
Ron Odey <ronodey83@outlook.com>
2020-04-26 14:52:34 -0500

    Module 3 - HOP03

---
+++ ON/RonaldOdeyemi/Module3/Operators-Routines/Increment-Decrement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Increment_Decrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a <= 10; a++)
+            {
+                Console.WriteLine(a);
+
+                /* I've created a For Loop argument within the Main Function. Within the For Loop, I have created an integer called "a" with an assigned value of 0. 
+                The argument looks at the boolean statement of a is less than or equal to 10. Everytime this statement is true, the value of "a" is displayed through the
+                Console.WriteLine(a) command and then the For Loop increments "a" by 1 using the post increment operator. The process repeats itself through this loop, each time
+                displaying the value of "a" on a new line through the "WriteLine" command ("Write" would have displayed each value next to each other). The For Loop terminates
+                when the statement of "a is less than or equal to 10" is false i.e. the final value displayed is 10 */
+                
+            }
+
+            for (int a = 15; a >= 1; a--)
+
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
---
+++ ON/RonaldOdeyemi/Module3/Operators-Routines/NumericOperators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NumericOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20; // I have created an integer variable called num1 and assigned the value of 20 into it
+            int num2 = 77; // I have created an integer variable called num2 and assigned the value of 77 into it
+            float num3 = 5.2f; // I have created an integer variable called num1 and assigned the value of 20 into it
+
+            float sum = num1 + num2 + num3; //  I have created a float variable type called "sum" and I'm assigning the value of the sum of num1, num2 and num3. The variable type is float because num3 is a decimal numerical value
+            int subtract = num2 - num1; // I have created an int variable called "subtract" and I'm assigning the value of calculated result of num2 minus num1 into "subtract"
+            int multiply = num1 * num2; // I have created an int variable called "multiply" and I'm assigning the value of calculated result of num1 multiplied by num2 into "multiply"
+            float divide = num1 / num3; // I have created a float variable called "divide" and I'm assigning the value of calculated result of num1 divided by num3 which will give me a decimal value that can't be assigned into an int type
+            int modulus = num2 % num1; // I have created an int variable called "modulus" and I'm assigning the value of calculated result of num2 modulus num1 which creates a remainder value of 17 (i.e. 77/20 = 60 remainder 17)
+
+            Console.WriteLine("Sum of 3 numbers is: " + sum); // This displays the value of the sum variable which is 102.2
+            Console.WriteLine("77 - 20 is: " + subtract); // This displays the value of the subtract variable which is 57
+            Console.WriteLine("20 * 77 is: " + multiply);// This displays the value of the multiply variable which is 1540
+            Console.WriteLine("20 / 5.2 is: " + divide);// This displays the value of the divide variable which is 3.846154
+            Console.WriteLine("77 % 20 is: " + modulus); // This displays the value of the subtract variable which is 17
+
+        }
+    }
+}
