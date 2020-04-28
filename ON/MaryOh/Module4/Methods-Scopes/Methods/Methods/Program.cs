69aa165ceaefa33939e58ed85760e84a83ff19d8
Mary Oh <mary.christine.oh@gmail.com>
2020-04-27 17:54:46 -0700

    HOP04: created method called PrintSomething

---
+++ ON/MaryOh/Module4/Methods-Scopes/Methods/Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        // this is a method called "PrintSomething"
+        // its function is to print a line of string "Hello World!"
+        public static void PrintSomething() {
+            Console.WriteLine("Hello World!");
+        }
+
+        // this is a method called "Main"
+        // this method is called when the program is ran
+        static void Main(string[] args)
+        {
+            PrintSomething(); // mmethod "PrintSomething" is called
+        }
+    }
+}
