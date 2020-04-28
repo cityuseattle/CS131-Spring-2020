d93c51f9282388b0fd322614aa3c4f1874e4b70e
Mary Oh <mary.christine.oh@gmail.com>
2020-04-27 18:21:00 -0700

    HOP04: Scope completed

--- ON/MaryOh/Module4/Methods-Scopes/Scopes/Scopes/Program.cs
+++ ON/MaryOh/Module4/Methods-Scopes/Scopes/Scopes/Program.cs
@@ -4,11 +4,12 @@ namespace Scopes
 {
     class Program
     {
+        public static string Str2;
+
         static void ReturnString(string Str1) {
-            string Str2 = "Programming in C#";
+            Str2 = "Programming in C#";
 
             Console.Write(Str1);
-            Console.WriteLine(Str2);
         }
 
         static void Main(string[] args)
