9f552ece2542f07f85cd7ee68c58157933479cda
Mary Oh <mary.christine.oh@gmail.com>
2020-04-27 18:16:09 -0700

    HOP04: created method called ReturnString

---
+++ ON/MaryOh/Module4/Methods-Scopes/Scopes/Scopes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Scopes
+{
+    class Program
+    {
+        static void ReturnString(string Str1) {
+            string Str2 = "Programming in C#";
+
+            Console.Write(Str1);
+            Console.WriteLine(Str2);
+        }
+
+        static void Main(string[] args)
+        {
+            ReturnString("CS131 ");
+        }
+    }
+}
