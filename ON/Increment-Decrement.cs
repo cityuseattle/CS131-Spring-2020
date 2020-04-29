2d5b99598822e0312a9fc90e0d7b73af9c583d8e
Juwan Flowers <juwanflowers@Juwans-MacBook-Pro.local>
2020-04-28 22:31:21 -0700

    HOP03

---
+++ ON/Increment-Decrement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Increment_Decrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a <= 10; a++)
+            {
+                Console.WriteLine(a);
+            }
+
+            for (int a = 15; a >= 1; a--)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
