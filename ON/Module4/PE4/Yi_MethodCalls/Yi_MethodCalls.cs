91cf5cf2ab9e32f82f667d544401b307aa90c965
David Yi <yidavid@cityuniversity.edu>
2020-04-30 18:53:18 -0700

    Yi PE4 MethodCalls

---
+++ ON/Module4/PE4/Yi_MethodCalls/Yi_MethodCalls.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Yi_MethodCalls
+{
+    class Program
+    {
+        /**The CalculateAverage method takes the three grades the G.Treele
+        obtained and calculates the average grade between the three **/
+        static float CalculateAverage(float grd1, float grd2, float grd3)
+        {
+            float average = (grd1 + grd2 + grd3) / 3;
+            return average;
+        }
+
+        /**The ShowAverage method takes G.Treele's name and the average
+          score and writes the added information**/
+        static void ShowAverage(string studname, float avegrd)
+        {
+            Console.WriteLine(studname + ": " + avegrd );
+        }
+        
+        static void Main()
+        {
+            const float MATH_GRADE = 80.0f;
+            const float BIOLOGY_GRADE = 80.0f;
+            const float PROGRAMMING_GRADE = 92.0f;
+            const string STUDENT_NAME = "G.Treele";
+            
+            //Call the CalculateAverage method 
+            float average = CalculateAverage(MATH_GRADE, BIOLOGY_GRADE, PROGRAMMING_GRADE);
+            
+            //Calls the ShowAverage method
+            ShowAverage(STUDENT_NAME, average);
+            Console.ReadLine();
+        }
+    }
+}
