a9e5f9f8e938857e45db0b3f37f3b207dbf1a22f
Juwan Flowers <juwanflowers@Juwans-MacBook-Pro.local>
2020-05-03 11:36:25 -0700

    HOP04 Scopes

---
+++ ON/Scopes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace scope
+{
+    class Program
+    {
+        public static string Str2;
+
+        static void ReturnString(string Str1)
+        {
+            string Str2 = "Programming in C#";
+
+            Console.Write(Str1);
+            Console.WriteLine(Str2);
+        }
+
+        static void Main(string[] args)
+        {
+            ReturnString(" CS131 ");
+            Console.WriteLine(Str2);
+        }
+    }
+}
