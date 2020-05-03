83afdfe9bc2d44757b8afa52f12b076483856c4b
Juwan Flowers <juwanflowers@Juwans-MacBook-Pro.local>
2020-05-03 11:35:06 -0700

    HOP04 Mthods

---
+++ ON/Method.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        //public static void PrintSomething()
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    //PrintSomething(); // the method PrintSomething is called here
+        //}
+
+        //public static int Adding(int num1, int num2)
+        //{
+        //    int sum = num1 + num2;// the integers are being added
+        //    return sum;// the value of sum is to be returned
+        //}
+
+        //static void Main(string[] args)// The method is called "Main"
+        //{
+        //    Console.WriteLine(Adding(10, 20));//The method "Adding" is being called and I gave num1 and num2 values.
+        //                                       // the result will be printed when the program is ran.
+        //}
+
+        public static int Mod(int a, int b)
+        { 
+            int modulus = a % b;// initialized the equation that will be ran in the program
+            return modulus; // the value of modulus is being returned
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mod(20, 13));// Gave "a" the value of 20 and "b" the value of 13.
+        }
+    }
+}
---
+++ ON/Types.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Distance = 77;// Gave integer the value of 77
+            float Length = 22.25f;// Gave float the value of 22.25
+            string Overall = "Value 100";// Gave string the value of 'Value 100
+
+            int FloatToInt = (int)Length;// Casted the float value to integer
+
+            Console.WriteLine(FloatToInt);
+            Console.WriteLine(Distance);
+            Console.WriteLine(Overall);
+            Console.WriteLine(Length);
+
+            Console.WriteLine("When you cast a double to an integer everything to the right of the decimal disappears and the big decimal now turns into a regular number");
+
+        }
+    }   
+}
