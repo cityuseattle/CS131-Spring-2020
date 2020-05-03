fe802267c9ac89caddd59030b678991214da208d
Ron Odey <ronodey83@outlook.com>
2020-05-03 13:34:07 -0500

    HOP04 - Methods and Scopes

---
+++ ON/RonaldOdeyemi/Module4/Methods-Scopes/Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        
+        public static int Mod(int a, int b)
+
+        {
+            int Modulus = a%b;
+            return Modulus;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mod(20,13));
+        }
+    }
+}
---
+++ ON/RonaldOdeyemi/Module4/Methods-Scopes/Scopes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Scopes
+{
+    class Program
+
+    {
+        public static string Str2;
+        static void ReturnString(string Strl)
+        {
+            Str2 = "Programming in C#";
+
+            Console.Write(Strl);
+        }
+                        
+        static void Main(string[] args)
+        {
+            ReturnString("CS131 ");
+            Console.WriteLine(Str2);
+
+        }
+    }
+}
