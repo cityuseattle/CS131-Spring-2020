f6d5c1c7cbb31452215f06dfb597ce55970b6c63
Jeremy <jkgrub@gmail.com>
2020-05-03 19:09:23 -0700

    Module 4

---
+++ ON/JeremyGrub/Module4/Methods-Scopes/Methods/Methods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        public static int Remainder(int num1, int num2)
+        {
+            int modulus = num1 % num2;
+            return modulus;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Remainder(20,13));
+        }
+    }
+}
