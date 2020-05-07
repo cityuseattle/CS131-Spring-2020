5c01ce52a0473121780b8ccafcd41daa8e5ec840
David Yi <yidavid@cityuniversity.edu>
2020-05-07 13:25:11 -0700

    Yi Module 5 PE5 Codes

---
+++ ON/Module5/PE5/Yi_GoQuit/Yi_GoQuit.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Yi_GoQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Obligatory statement about utilizing narrative that is quoted
+            Console.WriteLine("The following dialog is from The Matrix (The First Movie)");
+            Console.WriteLine("The ending statement when you quit is from Matrix the Game");
+            Console.WriteLine("The references are listed at the bottom of the code, commented.");
+            Console.WriteLine("Do you want to proceed? (Y/N)");
+
+            bool response = true;
+
+            while (response)
+            {
+                string Answer = Console.ReadLine();
+                if (Answer == "Y" || Answer == "y") //checks the response of the user if they want to proceed.
+                {
+                    Console.WriteLine("Excellent. Let's proceed.");
+                    ShowMenu();
+                    string choice = Console.ReadLine();
+                    if (choice == "1")
+                    {
+                        Console.WriteLine("I'm sorry you won't be joining us.");
+                        Environment.Exit(0);//This code exits the program
+                    }
+                    else if (choice == "2")
+                    {
+                        Console.WriteLine("Follow me.");
+                        Environment.Exit(0);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("I'm sorry you won't be joining us.");
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        //Shows the menu 
+        static void ShowMenu()
+        {
+            //The actual menu. Blue pill means stop, Red pill means go.
+            Console.WriteLine("**This is your last chance. After this there is no turning back.**");
+            Console.WriteLine("1. You take the blue pill, the story ends; you wake up " +
+                              "in bed and believe whatever you want to believe.");
+            Console.WriteLine("2. You take the red pill, you stay in Wonderland " +
+                              "and I show you how deep the rabbit hole goes. " +
+                              "Remember, all I'm offering is the truth. Nothing more. ");
+        }
+    }
+}
+
+/* Citations follow:
+ * 
+ * Matrix. (1999)
+ *
+ * The Matrix, Path of Neo, Shiny Entertainment. (2005)
+*/
---
+++ ON/Module5/PE5/Yi_Warnlvl_Bool/Yi_WarnLvlBool.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Yi_Warnlvl_Bool
+{
+    class Program
+    {
+        //Create Constant Integers of the warning levels
+        const int NoEmerg = -1;
+        const int LVL0 = 0, LVL1 = 1, LVL2 = 2, LVL3 = 3; //Identifies the levels of risk
+        const bool Warning = true; //Anything other than noEmerg has a true warning type
+        
+        static void Main(string[] args)
+        {
+            /*The Structure for the blocks of code in the Main is as follows:
+             * Warning Level type
+             * rainIndex
+             * Call RainStorm method (Displays how much rain is expected)
+             * Call WarningType method (Displays the severity of the incoming hurricane)
+             */
+            
+            int Level = NoEmerg;
+            int rainIndex = 3; //All rainIndex is in cm/hr
+            RainStorm(rainIndex, !Warning);
+            WarningType(Level);
+
+            Level = LVL0;
+            rainIndex = 3;
+            RainStorm(rainIndex, Warning);
+            WarningType(Level);
+
+            Level = LVL1;
+            rainIndex = 7;
+            RainStorm(rainIndex, Warning);
+            WarningType(Level);
+            
+            Level = LVL2;
+            rainIndex = 20;
+            RainStorm(rainIndex, Warning);
+            WarningType(Level);
+
+            Level = LVL3;
+            rainIndex = 60;
+            RainStorm(rainIndex, Warning);
+            WarningType(Level);
+        }
+        //This method lists all of the levels and their warning types.
+        public static void WarningType(int Level)
+        {
+            if (Level == LVL3) // Checks for the Level type
+            {
+                Console.WriteLine("Warning! Evacuate now! Hurricane is closing in!");
+            }
+            else if (Level == LVL2)
+            {
+                Console.WriteLine("Warning! Prepare for evacuation. Hurricane imminent!");
+            }
+            else if (Level == LVL1)
+            {
+                Console.WriteLine("Warning! Hurricane is developing over the ocean");
+            }
+            else if (Level == LVL0)
+            {
+                Console.WriteLine("Storm is developing over the ocean. We are monitoring the situation.");
+            }
+            else
+            {
+                Console.WriteLine("No issues detected. Enjoy your day!");
+            }
+        }
+        
+        //This method is the Boolean section.
+        //This method reads the rain index and displays the message
+        static public void RainStorm(int rainIndex, bool Warning)
+        {
+            Console.WriteLine("Rain Index: " + rainIndex + " cm/hr."); //States the rain index level
+            
+            if (rainIndex < 5 && !Warning) //Checks for parameters to display certain messages.
+            {
+                Console.WriteLine("Light sprinkles expected.");
+            }
+            else if (rainIndex < 5 && Warning)
+            {
+                Console.WriteLine("Light rain expected.");
+            }
+            else if (rainIndex < 10 && Warning)
+            {
+                Console.WriteLine("Moderate rain expected.");
+            }
+            else if (rainIndex > 10 && rainIndex < 30 && Warning)
+            {
+                Console.WriteLine("Heavy rain expected.");
+            }
+            else if (rainIndex > 30 && Warning)
+            {
+                Console.WriteLine("Severe rain expected.");
+            }
+        }
+    }
+}
