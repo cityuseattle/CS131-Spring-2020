d2dce1ea2e44d9fb83acf0cfeae02f71b5718bc6
Ron Odey <ronodey83@outlook.com>
2020-05-09 14:35:02 -0500

    Committing module 5 - fun times!

---
+++ ON/RonaldOdeyemi/Module5/Conditions-Loops/While-Loop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+            Console.WriteLine("Guess a number (1 - 10): ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            while (true)
+            {
+
+                if (userInput > 0 && userInput <= 10)
+                {
+                    if (userInput == num)
+                    {
+                        Console.WriteLine("Your guess was correct!");
+                        break;
+                    }
+
+                    else 
+                    {
+                        Console.WriteLine("You guessed wrong. Guess again: ");
+                        userInput = Convert.ToInt32(Console.ReadLine());
+                    }
+                    
+                }
+                else if (userInput < 0 || userInput > 10)
+                {
+                    Console.WriteLine("Number must be between 1 – 10, guess again: ");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+
+                }
+            }
+        }
+    }
+}
