042ec3de3bef36d0b0f86c111daf3a6e9ff4b53d
Mary Oh <mary.christine.oh@gmail.com>
2020-05-11 15:49:04 -0700

    Module 6: Created file called String and used arithmetic operator to concatenate

---
+++ ON/MaryOh/Module6/String-Manipulation/String/String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Welcome = "Welcome to ";
+            string CourseName = "CS131";
+            // Concatenate string
+            string message = Welcome + CourseName;
+
+            Console.WriteLine(message);
+        }
+    }
+}
