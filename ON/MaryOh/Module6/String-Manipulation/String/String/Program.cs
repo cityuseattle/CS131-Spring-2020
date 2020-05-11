51ee42e3e4f31c02450f3391a4716ec48854cd40
Mary Oh <mary.christine.oh@gmail.com>
2020-05-11 15:55:42 -0700

    Module 6: Updated file String - utilize ToUpper and ToLower

--- ON/MaryOh/Module6/String-Manipulation/String/String/Program.cs
+++ ON/MaryOh/Module6/String-Manipulation/String/String/Program.cs
@@ -8,13 +8,10 @@ namespace String
         {
             string Welcome = "Welcome to ";
             string CourseName = "CS131";
-            // Concatenate string using concat() method
-            string message1 = string.Concat(Welcome, CourseName);
-            // Concatenate string by substitutes values of variables into placeholders in a string
-            string message2 = $"Printing strings for the second time: {Welcome} {CourseName}";
+            // Concatenate string
+            string message = Welcome.ToUpper() + CourseName.ToLower();
 
-            Console.WriteLine(message1);
-            Console.WriteLine(message2);
+            Console.WriteLine(message);
         }
     }
 }
