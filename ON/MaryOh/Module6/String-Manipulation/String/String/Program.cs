caaa34b6926e70929213ebfb0998931eb7cfb727
Mary Oh <mary.christine.oh@gmail.com>
2020-05-11 16:04:10 -0700

    Module 6: Updated file String - utilize for loop to print each character in new line

--- ON/MaryOh/Module6/String-Manipulation/String/String/Program.cs
+++ ON/MaryOh/Module6/String-Manipulation/String/String/Program.cs
@@ -8,13 +8,14 @@ namespace String
         {
             string Welcome = "Welcome to ";
             string CourseName = "CS131";
-            // Concatenate string using concat() built in method
-            string message = string.Concat(Welcome, CourseName);
+            // Concatenate string
+            string message = Welcome + CourseName;
 
-            // Prints the character at index 0 of the string Welcome
-            Console.WriteLine(Welcome[0]);
-            // Prints the last character of string CourseName
-            Console.WriteLine(CourseName.Substring(CourseName.Length - 1));
+            // Iterate through string message
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.WriteLine(message[i]);
+            }
         }
     }
 }
