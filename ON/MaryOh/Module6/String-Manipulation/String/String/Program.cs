7eea7db8407bba4cd4da8d530c9a310c370287e5
Mary Oh <mary.christine.oh@gmail.com>
2020-05-11 15:53:07 -0700

    Module 6: Updated file String to use concat method and placeholders

--- ON/MaryOh/Module6/String-Manipulation/String/String/Program.cs
+++ ON/MaryOh/Module6/String-Manipulation/String/String/Program.cs
@@ -8,10 +8,13 @@ namespace String
         {
             string Welcome = "Welcome to ";
             string CourseName = "CS131";
-            // Concatenate string
-            string message = Welcome + CourseName;
+            // Concatenate string using concat() method
+            string message1 = string.Concat(Welcome, CourseName);
+            // Concatenate string by substitutes values of variables into placeholders in a string
+            string message2 = $"Printing strings for the second time: {Welcome} {CourseName}";
 
-            Console.WriteLine(message);
+            Console.WriteLine(message1);
+            Console.WriteLine(message2);
         }
     }
 }
