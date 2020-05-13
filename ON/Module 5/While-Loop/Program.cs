6aa85c0139de0ea5e855344ace8d51f65e9ca6a4
Marshall Townsend <Marshall@cityuniversity.edu>
2020-05-12 21:45:39 -0700

    While Loops

---
+++ ON/Module 5/While-Loop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+            Console.WriteLine("Guess a number (1 - 10): ");
+            int userInput = Convert.ToInt32(Console.ReadLine()); //receive user input and store in "userInput" variable
+
+            while (userInput > 0 && userInput <= 10)
+                {
+                    if (userInput == num)
+                    {
+                        Console.WriteLine("Your Guess was Correct!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You Guessed Wrong, Guess Again:");
+                        userInput = Convert.ToInt32(Console.ReadLine());
+                    if (userInput >= 11)
+                        Console.WriteLine("Number must be between 1 and 10, Guess Again!");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+
+                }
+            }
+        }
+    }
+}
