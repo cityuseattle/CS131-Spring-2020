574e0c8b988df15671cdf35ec357a906a2dd454b
Marshall Townsend <Marshall@cityuniversity.edu>
2020-05-12 21:11:11 -0700

    Conditional Statements

---
+++ ON/Module 4/ScopeLevels/ScopeProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ScopeLevels
+{
+    class Program
+    {
+        static int classAndMethodLevel = 100; //Class level declaration
+        static int classAndBlockLevel = 200; 
+        static void Main(){
+            Console.WriteLine("Class Level: " + classAndMethodLevel);
+            Console.WriteLine("Class Level: " + classAndBlockLevel);
+            ShowLocalizedValues();
+            Console.ReadLine();
+        }
+        //Receive uninitialized interest rate explicitly by reference.
+        static void ShowLocalizedValues()
+        {
+            const bool ENTER_CONDITION = true;
+            int classAndMethodLevel = 25; //method level declaration.
+            Console.WriteLine("Method Level: " + classAndMethodLevel);
+            if (ENTER_CONDITION)
+            {
+                int classAndBlockLevel = 5; //block level declaration
+                Console.WriteLine("Block Level: " + classAndBlockLevel);
+
+            }
+        }
+    }
+}
---
+++ ON/Module 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Conditional_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            
+                Console.WriteLine("Guess a number (1-10): ");
+                int userInput = Convert.ToInt32(Console.ReadLine()); // reveieve user input and store
+
+                if (userInput == num)
+                {
+                    Console.WriteLine("Your guess was correct!");
+                }
+            
+                else if (userInput >= 10)
+            {
+                Console.WriteLine("Hey, The number must be between 1 and 10. Try Again!");
+            }
+        }
+    }
+}
