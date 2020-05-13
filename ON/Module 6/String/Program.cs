16b35d454dc851e2cddbe51dd21bc56fbf710e13
Marshall Townsend <Marshall@cityuniversity.edu>
2020-05-12 22:57:46 -0700

    Strings, having rough time figuring out the last part but Ill keep working on it...

---
+++ ON/Module 6/String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Welcome = "Welcome to ";
+            string CourseName = "CS131";
+            string message = Welcome + CourseName;
+
+            //the index starts at 0 and keeps iterating as long as its in range of "message"
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.WriteLine(message[i]);
+            }
+                     
+        }
+    }
+}
