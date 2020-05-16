dc468ea13f3e938760ad34c1b51bc0c22267caff
Ron Odey <ronodey83@outlook.com>
2020-05-16 12:36:37 -0500

    Committing HOP06

---
+++ ON/RonaldOdeyemi/Module6/String-Manipulation/String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Welcome = "Welcome to ";
+            string CourseName = "CS131";
+            string message = Welcome + CourseName;
+
+            for (int i = 0; i < message.Length; i++)
+            {
+               if (i%2==0)
+                Console.Write(message[i]);
+            }
+
+        }
+    }
+}
