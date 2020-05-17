9292613ae05092328946fbea2235badf3e371ea9
Jeremy <jkgrub@gmail.com>
2020-05-17 15:24:04 -0700

    Module 6

---
+++ ON/JeremyGrub/Module6/String-Manipulation/String/String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Welcome = "Welcome to ";
+            string CourseName = "CS131";
+            string message = Welcome + CourseName;
+
+            for (int i = 0; i < message.Length; i++)
+               
+            {
+                if (i%2 == 0)
+                {
+                    Console.Write(message[i]);
+                }
+            }
+        }
+    }
+}
