9f284df82b0ac46f13e18b385bab0e69a430bb62
TrevorCanCode <63311674+TrevorCanCode@users.noreply.github.com>
2020-05-17 14:48:42 -0400

    Add files via upload

---
+++ ON/Module 6/String Manipulation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace String_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create string variables
+            string Welcome = "Welcome to ";
+            string CourseName = "CS131";
+            string message = Welcome + CourseName;
+
+            //use for loop to iterate over each letter in message
+            // for the legnth of the message variable
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    // write out each letter at the index if i % 2 is 0
+                    Console.Write(message[i]);
+                }
+
+            }
+
+        }
+    }
+}
