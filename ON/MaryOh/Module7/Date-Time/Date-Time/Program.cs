2e63b8fe5167fcc5a81d3d77d041d96088a0b586
Mary Oh <mary.christine.oh@gmail.com>
2020-05-18 11:50:45 -0700

    Module 7: HOP07 - Created method called ShowTime()

---
+++ ON/MaryOh/Module7/Date-Time/Date-Time/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Date_Time
+{
+    class Program
+    {
+        void ShowTime()
+        {
+            DateTime currentDate = DateTime.Now;
+            bool leapYear = DateTime.IsLeapYear(2020);
+
+            Console.WriteLine(currentDate);
+            Console.Write("2020 is a leap year: ");
+            Console.WriteLine(leapYear);
+        }
+
+        static void Main(string[] args)
+        {
+            // create an object of class program
+            // access a method of that class
+            Program MyProgram = new Program();
+
+            MyProgram.ShowTime();
+        }
+    }
+}
