3d7f57692af7df0f14eb6c41ee9f8e23780081b4
Priyanka Kamdar <pkamdar27@gmail.com>
2020-05-17 22:41:08 -0700

    HOP06 - String Manipulation

---
+++ ON/PriyankaKamdar/Module6/MailString/MailString/MailStringProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace MailString
+{
+    class MailStringProgram
+    {
+        public static string GenerateEmail(string firstName,string lastName)
+        {
+            string id = "@abc.com";
+            string firstCharacters = firstName.Substring(0, 1);
+            string lastCharacters = lastName.Substring(0, 3);
+            string emailAddress = firstCharacters + lastCharacters + id;
+            return emailAddress;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GenerateEmail("priyanka","kamdar"));   //Calling GenerateEmail method and displaying result
+            Console.WriteLine(GenerateEmail("vivek", "kamdar"));     //Calling GenerateEmail method and displaying result
+            Console.WriteLine(GenerateEmail("darshan", "botadra"));   //Calling GenerateEmail method and displaying result
+        }
+
+    }
+}
---
+++ ON/PriyankaKamdar/Module6/String-Manipulation/String/String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Welcome = "Welcome to ";                //variable string declared
+            string CourseName = "CS131";                  //variable string declared
+            string message = Welcome + CourseName;        //concatenate string    
+
+            for (int i = 0; i < message.Length; i++)       
+            {
+                if ((i % 2) == 0)
+                {
+                  Console.Write(message[i]);               //prints the value starting at even index
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
