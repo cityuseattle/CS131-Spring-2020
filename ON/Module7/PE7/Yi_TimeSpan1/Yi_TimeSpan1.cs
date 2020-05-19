05623bb20595bf172d6ecdd124dd12efb896fdaa
David Yi <yidavid@cityuniversity.edu>
2020-05-19 12:50:39 -0700

    Yi_PE7_TimeSpan1

---
+++ ON/Module7/PE7/Yi_TimeSpan1/Yi_TimeSpan1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Yi_TimeSpan1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initiate the parameters for timespan
+            const int DAY = 1;
+            const int HOUR = 23;
+            const int MINUTE = 42;
+            const int SECONDS = 35;
+            
+            //Instantiate the timespan 
+            TimeSpan Daytime = new TimeSpan(DAY, HOUR, MINUTE, SECONDS);
+            
+            //Print out the timespan
+            Console.WriteLine(
+                $"{Daytime.Days} days, {Daytime.Hours} hours, {Daytime.Minutes} minutes, {Daytime.Seconds} seconds.");
+        } 
+    }
+}
