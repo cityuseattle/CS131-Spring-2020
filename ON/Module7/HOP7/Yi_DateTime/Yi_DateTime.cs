220ddaf1a78d26c5d7f407119b74175589510831
David Yi <yidavid@cityuniversity.edu>
2020-05-19 11:52:02 -0700

    Yi_Module7_HOP

---
+++ ON/Module7/HOP7/Yi_DateTime/Yi_DateTime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Yi_DateTime
+{
+    class Program
+    {
+        void ShowTime()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("What year were you born?: ");
+            int birthYear = Convert.ToInt32(Console.ReadLine());
+
+            int age = (Convert.ToInt32(DateTime.Now.Year) - birthYear);
+            
+            //Challenge Portion
+            int year = DateTime.Now.Year;
+            int month = DateTime.Now.Month;
+            
+            Console.WriteLine("Here is what we know from your inputs: ");
+            Console.WriteLine("Your name: " + name);
+            Console.WriteLine("Today is: " + DateTime.Now.ToShortDateString());
+            Console.WriteLine("Your age: " + age);
+            //Displays the days in the current month
+            Console.WriteLine($"The amount of days in {month}, {year} are {DateTime.DaysInMonth(year,month)} days.");
+        }
+        static void Main(string[] args)
+        {
+            Program MyProgram = new Program();
+
+            MyProgram.ShowTime();
+        }
+    }
+}
