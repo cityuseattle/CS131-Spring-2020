0154558095580d2e131df5ef22b33e85cee9581d
David Yi <yidavid@cityuniversity.edu>
2020-05-20 10:38:57 -0700

    Yi PE7

---
+++ ON/Module7/PE7/Yi_Birthday/Yi_Birthday.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Yi_Birthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //introduce variables to describe birthday
+            const int MTH = 4;
+            const int DY = 29;
+            const int YR = 1994;
+            const int HR = 8;
+            const int MIN = 31;
+            const int SEC = 42;
+            
+            //establish Date time to display my birthday
+            DateTime BDay = new DateTime(YR, MTH, DY, HR, MIN, SEC);
+            ShowBirthday(BDay);
+        }
+        
+        //Method that displays Birthday
+        public static void ShowBirthday(DateTime DT)
+        {
+            Console.WriteLine("David's Birth Information: ");
+            Console.WriteLine($"Year: {DT.Year}\nMonth: {DT.Month}\nDay: {DT.Day}\n" +
+                              $"Hour: {DT.Hour}\nMinutes: {DT.Minute}\nSeconds: {DT.Second}");
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ON/Module7/PE7/Yi_TimeSpan2/Yi_TimeSpan2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Yi_TimeSpan2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables for Weekly Programming Hours
+            int WkProgDay = 4;
+            int WkProgHour = 2;
+            int WkProgMin = 32;
+            int WkProgSec = 45;
+            
+            //Variables for Weekly Testing Hours
+            int WkTestDay = 2;
+            int WkTestHour = 13;
+            int WkTestMin = 45;
+            int WkTestSec = 23;
+            
+            //Writes out time span for both the weekly programming and testing hours
+            TimeSpan WkProg = new TimeSpan(WkProgDay, WkProgHour, WkProgMin, WkProgSec);
+            TimeSpan WkTest = new TimeSpan(WkTestDay, WkTestHour,WkTestMin,WkTestSec);
+            
+            //Adds the two time spans together to get total work time
+            TimeSpan TtlDevTime = (WkProg + WkTest);
+            
+            //Writes the data.
+            Console.WriteLine($"The total development time for this project was: {TtlDevTime.Days} days, " +
+                              $"{TtlDevTime.Hours} hours, {TtlDevTime.Minutes} minutes, and {TtlDevTime.Seconds} seconds.");
+
+        }
+    }
+}
