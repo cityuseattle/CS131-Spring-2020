f3f9e84182e478bc523128aeec11f5cfc2fbe94f
Jeremy <jkgrub@gmail.com>
2020-05-20 10:23:32 -0700

    Module 7

---
+++ ON/JeremyGrub/Module7/Date-Time/Date-Time/Date-Time/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDate = DateTime.Now;
+            int numDays = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+            Console.WriteLine(currentDate.Month);
+            Console.WriteLine(numDays); 
+        }
+    }
+}
