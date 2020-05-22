21968ed327785745e73658d89f62a9385063070e
Priyanka Kamdar <pkamdar27@gmail.com>
2020-05-22 07:55:48 -0700

    HOP07

---
+++ ON/PriyankaKamdar/Module7/Birthday/Birthday/BirthdayClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Birthday
+{
+    class BirthdayClass
+    {
+        static void Main(string[] args)
+        {
+            const int YEAR = 1990, MONTH = 01, DAY = 30, HRS = 12, MIN = 20, SEC = 40; //Constants for birthdate.
+            DateTime birthDay = new DateTime(YEAR, MONTH, DAY, HRS, MIN, SEC); //create DateTime.
+            showBirthDay(birthDay);
+            Console.WriteLine("Birthday in long date format: {0}", birthDay.ToLongDateString()); //Displays date time in long date format.
+        }
+
+        static void showBirthDay(DateTime birthDay)
+        {
+            Console.WriteLine("Year : {0}, Month: {1}, Day: {2}", birthDay.Year, birthDay.Month, birthDay.Day); //Displays year, month and day.
+            Console.WriteLine("DayOfTheWeek : {0}, DayofTheYear: {1}", birthDay.DayOfWeek, birthDay.DayOfYear); //Displays day of the week and year.
+            Console.WriteLine("BirthTimeHr : {0}, BirthTimeMin: {1}", birthDay.Hour, birthDay.Minute); //displays hour and minute of DateTime.
+        }
+    }
+}
---
+++ ON/PriyankaKamdar/Module7/Date_Time/Date_Time/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Date_Time
+{
+    class Program
+    {
+        void ShowTime()
+        {
+            DateTime currentDateTime = DateTime.Now; //Creates current DateTime object 
+            Console.WriteLine(currentDateTime.Month);  //Displays current month.
+            Console.WriteLine(DateTime.DaysInMonth(currentDateTime.Year, currentDateTime.Month)); //gets no of days in current month.
+        }
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.ShowTime();
+        }
+    }
+}
