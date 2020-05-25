92afaee3bf9fe23b54a3b77f04f3c3c9ec782126
Mary Oh <maryoh@marys-mbp.lan>
2020-05-25 15:37:26 -0700

    Module 8: HOP08 - Class and Object challenge

--- ON/MaryOh/Module 8/OOP/Class-Object/Class-Object/Program.cs
+++ ON/MaryOh/Module 8/OOP/Class-Object/Class-Object/Program.cs
@@ -17,6 +17,15 @@ namespace ClassObject
             double Area = l * w;
             return Area;
         }
+
+        public double CalCircleArea(double r)
+        {
+            radius = r;
+            const double PI = 3.14;
+
+            double Area = PI * r * r;
+            return Area;
+        }
     }
 
     class Program
@@ -26,17 +35,23 @@ namespace ClassObject
             double RecWidth = 7;
             double RecLength = 8;
             double SqWidth = 2;
+            double rad = 3;
 
             // Create an instance/object of class Shapes called Rectangle
             Shapes Rectangle = new Shapes();
             // Create an instance/object of class Shapes called Square
             Shapes Square = new Shapes();
+            // Create an instance/object of class Shapes called Circle
+            Shapes Circle = new Shapes();
 
             // Use object Rectangle to call method CalArea of class Shapes
             Console.WriteLine("Area of rectangle is: " + Rectangle.CalArea(RecLength, RecWidth));
 
             // Use object Square to call method CalArea of class Shapes
             Console.WriteLine("Area of square is: " + Square.CalArea(SqWidth, SqWidth));
+
+            // Use object Circle to call method CalCircleArea of class Shapes
+            Console.WriteLine("Area of circle is: " + Circle.CalCircleArea(rad));
         }
     }
 }
