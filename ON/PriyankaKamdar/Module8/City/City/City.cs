50a79439663c5b3a538fb47a1e15ce3ea18224b3
Priyanka Kamdar <pkamdar27@gmail.com>
2020-05-31 22:49:01 -0700

    HOP08

---
+++ ON/PriyankaKamdar/Module8/City/City/City.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace City
+{
+    class City
+    {
+        private string name;
+        private int population;
+
+        public City (string cityName, int cityPopulation)
+        {
+            name = cityName;
+            population = cityPopulation;
+        }
+
+        public string GetCity()
+        {
+            return name;
+        }
+
+        public int GetPopulation()
+        {
+            return population;
+        }
+        static void Main(string[] args)
+        {
+            City city = new City("Seattle", 1000000);
+            Console.WriteLine("City name is: " +  city.GetCity());
+            Console.WriteLine("City population is: " +  city.GetPopulation());
+        }
+    }
+}
---
+++ ON/PriyankaKamdar/Module8/OOP/Class-Object/Class-Object/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace Class_Object
+{
+    class Shapes
+    {
+        public double length;
+        public double width;
+        public double radius;
+        public double pie;
+
+        public double CalArea(double l, double w)
+        {
+            length = l;
+            width = w;
+            double Area = w * l;
+            return Area;
+        }
+
+        public double CalCircleArea(double r, double p)
+        {
+            radius = r;
+            pie = p;
+            double CircleArea = pie * radius * radius;
+            return CircleArea;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double RecWidth = 7;
+            double RecLength = 8;
+            double SqWidth = 2;
+            double CircleRadius = 3;
+            double CirclePie = 3.15;
+
+            Shapes Rectangle = new Shapes();
+            Shapes Square = new Shapes();
+            Shapes Circle = new Shapes();
+
+            Console.WriteLine("Area of Rectangle is: " + Rectangle.CalArea(RecLength, RecWidth));
+            Console.WriteLine("Area of Square is: " + Square.CalArea(SqWidth, SqWidth));
+            Console.WriteLine("Area of Circle is: " + Circle.CalCircleArea(CircleRadius, CirclePie));
+   
+        }
+    }
+}
