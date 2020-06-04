bb4e8dad0285fb1a48fe44ca11543a30b0ab7afd
David Yi <yidavid@cityuniversity.edu>
2020-06-04 14:20:22 -0700

    Yi_Module9_PE

---
+++ ON/Module9/PE9/Yi_Banking/Yi_Banking.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Yi_Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal OPENING_BALANCE = 55m;
+            const decimal MONTHLY_INTEREST = 0.0033m;
+            
+            //Savings object is instantiated
+            Savings savings = new Savings(OPENING_BALANCE);
+            savings.AddMonthlyInterest(MONTHLY_INTEREST);
+            savings.ShowBalance();
+
+            //Checking object is instantiated
+            Checking checkings = new Checking(OPENING_BALANCE);
+            checkings.DeductServiceCharge();
+            checkings.ShowBalance();
+    
+            //JointSavings object is instantiated
+            JointSavings jtsavings = new JointSavings("David", "Adrianna", OPENING_BALANCE);
+            jtsavings.AddMonthlyInterest(MONTHLY_INTEREST);
+            jtsavings.ShowBalance();
+            Console.ReadLine();
+            
+        }
+    }
+
+    class Account
+    {
+        //protected variables to ensure proper usage across classes.
+        protected decimal BankBalance { set; get; }
+        protected string FirstNameA { set; get; }
+        protected string FirstNameB { set; get; }
+
+        protected Account()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Inside the Account constructor.");
+        }
+
+        public void ShowBalance()
+        {
+            Console.WriteLine($"The balance is: {BankBalance.ToString("C")}");
+        }
+    }
+
+    //Creates the Savings child class
+    class Savings : Account
+    {
+        public Savings(decimal balance)
+        {
+            Console.WriteLine("==> Savings Constructor.");
+            BankBalance = balance;
+        }
+        
+
+        public void AddMonthlyInterest(decimal interest)
+        {
+            BankBalance *= (1.0m + interest);
+        }
+    }
+    
+    //Creates the JointSavings class which inherits data from Savings
+    class JointSavings : Savings
+    {
+        public JointSavings(string firstnameA, string firstnameB, decimal balance) : base(balance)
+        {
+            Console.WriteLine("====> Joint Savings Constructor");
+            BankBalance = balance;
+            FirstNameA = firstnameA;
+            FirstNameB = firstnameB;
+            Console.WriteLine($"The account holders are: {firstnameA} and {firstnameB}");
+        }
+    }
+
+    //Creates the child class Checking
+    class Checking : Account
+    {
+        public Checking(decimal balance)
+        {
+            Console.WriteLine("==> Checking constructor.");
+            BankBalance = balance;
+        }
+        public void DeductServiceCharge()
+        {
+            const decimal CHECKING_FEE = 1.50m;
+            BankBalance -= CHECKING_FEE;
+        }
+    }
+}
---
+++ ON/Module9/PE9/Yi_Fashion/Yi_Fashion.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Yi_Fashion
+{
+    //calls Fashion class
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fashion fashionDep = new Fashion();
+            fashionDep.DisplayPromotions();
+            Console.ReadLine();
+        }
+    }
+    
+    //Stores sales event data
+    class Promotion
+    {
+        public string Name { get; private set; }
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+
+        public Promotion(DateTime start, DateTime end, string name)
+        {
+            Start = start;
+            End = end;
+            Name = name;
+            
+        }
+    }
+
+    //Creates the sales output
+    abstract class Department
+    {
+        public abstract string DepartmentName { get; protected set; }
+        protected Promotion[] sales;
+        protected abstract void AssignPromotions();
+
+        public virtual void DisplayPromotions()
+        {
+            Console.WriteLine("{0} Department Promotions: ", DepartmentName);
+            foreach (Promotion sale in sales)
+            {
+                Console.WriteLine($"Name: {sale.Name}");
+                Console.WriteLine($"Start: {sale.Start.ToString("m")}");
+                Console.WriteLine($"End: {sale.End.ToString("m")}");
+            }
+            
+        }
+        
+    }
+
+    //Child class of Department
+    class Fashion : Department
+    {
+        public override string DepartmentName { get; protected set; }
+        public Fashion()
+        {
+            DepartmentName = "Fashion";
+            AssignPromotions();
+        }
+
+        protected override void AssignPromotions()
+        {
+            DateTime Start = new DateTime(2014, 9, 1);
+            DateTime End = new DateTime(2014, 9, 15);
+            Promotion promotion = new Promotion(Start, End, "Fall Sale");
+            sales = new Promotion[] {promotion};
+        }
+    }
+}
