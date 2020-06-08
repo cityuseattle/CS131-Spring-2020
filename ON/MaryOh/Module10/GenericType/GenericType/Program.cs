e0f474fc2da6f742f5ddcc30c2825b7f09133253
Mary Oh <maryoh@marys-mbp.lan>
2020-06-08 15:28:47 -0700

    Module 10: HOP10 - created file called GenericType

---
+++ ON/MaryOh/Module10/GenericType/GenericType/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericType
+{
+    class MainClass
+    {
+        // Declaring class StoreAnything using generic type
+        class StoreAnything<T, U>
+        {
+            public T var1 { set; get; }
+            public U var2 { set; get; }
+        }
+
+        public static void Main(string[] args)
+        {
+            // Create new object of class StoreAnything and pass a string type
+            StoreAnything<string, int> MyVar1 = new StoreAnything<string, int> { var1 = "CS", var2 = 131 };
+
+            Console.WriteLine(MyVar1.var1 + " " + MyVar1.var2);
+        }
+    }
+}
