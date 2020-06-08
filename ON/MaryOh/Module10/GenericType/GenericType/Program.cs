c93a8103463027bb1023f71280a8c534e3d2c84f
Mary Oh <maryoh@marys-mbp.lan>
2020-06-08 15:31:18 -0700

    Module 10: HOP10 - GenericType Challenge

--- ON/MaryOh/Module10/GenericType/GenericType/Program.cs
+++ ON/MaryOh/Module10/GenericType/GenericType/Program.cs
@@ -5,18 +5,19 @@ namespace GenericType
     class MainClass
     {
         // Declaring class StoreAnything using generic type
-        class StoreAnything<T, U>
+        class StoreAnything<T, U, V>
         {
             public T var1 { set; get; }
             public U var2 { set; get; }
+            public V var3 { set; get; }
         }
 
         public static void Main(string[] args)
         {
             // Create new object of class StoreAnything and pass a string type
-            StoreAnything<string, int> MyVar1 = new StoreAnything<string, int> { var1 = "CS", var2 = 131 };
+            StoreAnything<string, int, string> MyVar1 = new StoreAnything<string, int, string> { var1 = "CS", var2 = 131, var3 = "!" };
 
-            Console.WriteLine(MyVar1.var1 + " " + MyVar1.var2);
+            Console.WriteLine(MyVar1.var1 + " " + MyVar1.var2 + MyVar1.var3);
         }
     }
 }
