ed88c7ca1ab1d51d87687ad23ea7e729b9efc66d
Mary Oh <maryoh@marys-mbp.lan>
2020-06-08 15:42:51 -0700

    Module 10: HOP10 - Updated Arraylist file

--- ON/MaryOh/Module10/ArrayList/ArrayList/Program.cs
+++ ON/MaryOh/Module10/ArrayList/ArrayList/Program.cs
@@ -18,6 +18,19 @@ namespace Arraylist
             // Add values to Array List using AddRange() method
             myArrList.AddRange(new ArrayList() { 20, 17.7, "This is another string" });
 
+            // Remove array list elements using RemoveRange(), RemoveAt(), Remove() methods
+            // Parameter is element value
+            myArrList.Remove(10);
+            // Array now have 5 elements
+
+            // Parameter is index
+            myArrList.RemoveAt(0);
+            // Array now have 4 elements
+
+            // Parameter is index
+            // Remove elements from index 1 to index 2
+            myArrList.RemoveRange(1, 2);
+
             // Print each value in the array list using foreach() method
             foreach(var value in myArrList)
             {
