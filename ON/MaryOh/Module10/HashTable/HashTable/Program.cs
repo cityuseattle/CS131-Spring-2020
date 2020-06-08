85977629670ab2b8d07266cb051616b46462b95a
Mary Oh <maryoh@marys-mbp.lan>
2020-06-08 16:01:41 -0700

    Module 10: HOP10 - Created file called HashTable

---
+++ ON/MaryOh/Module10/HashTable/HashTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTable
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Initialize hash table using built in library collections
+            Hashtable myHashTable = new Hashtable()
+            {
+                // key, value pairs
+                {"Kim", 26 },
+                {"John", 77 },
+                {"Max", 50 }
+            };
+
+            // Obtaining values of keys
+            int HashTableVal1 = (int)myHashTable["Kim"];
+            int HashTableVal2 = (int)myHashTable["John"];
+            int HashTableVal3 = (int)myHashTable["Max"];
+
+            Console.WriteLine(HashTableVal1);
+            Console.WriteLine(HashTableVal2);
+            Console.WriteLine(HashTableVal3);
+        }
+    }
+}
