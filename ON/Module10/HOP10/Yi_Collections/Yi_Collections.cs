e2bffca5684e9625e1dc27b5c2499517adba0477
David Yi <yidavid@cityuniversity.edu>
2020-06-09 16:07:46 -0700

    Yi_HOP10

---
+++ ON/Module10/HOP10/Yi_Collections/Yi_Collections.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace Yi_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Array list using built in library
+            ArrayList myArrList = new ArrayList();
+            
+            //Add values to Array List using Add() method
+            myArrList.Add(10);
+            myArrList.Add(11.5);
+            myArrList.Add("This is a string");
+            
+            //Add values to Array List using AddRange() method
+            myArrList.AddRange(new ArrayList() {20, 17.7, "This is another String"});
+            
+            //revmoe array list elements using RemoveRange() and RemoveAt() and remove() methods
+            myArrList.Remove(10); //parameter is element value, in this case remove elements value of 10
+            //Now this array list has total of 5 elements
+
+            myArrList.RemoveAt(0); //Parameter is index number, in this case remove element at index 0, which is now 11.5
+            //now this array list has 4 elements total
+
+            myArrList.RemoveRange(1, 2); //parameter is index numbers, in this case remove elements from index 1 to before 2
+            
+            //print each value in the array list using foreach() method
+            foreach (var value in myArrList)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
---
+++ ON/Module10/HOP10/Yi_GenericType/Yi_GenericType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Yi_GenericType
+{
+    class Program
+    {
+        class StoreAnything<T, U, V> //declare class StoreAnything using generic type
+        {
+            public T var1 { set; get; }
+            public U var2 { set; get; }
+            public V var3 { set; get; }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            //Create new object of StoreAnything class, passing string and int type
+            StoreAnything<string, int, char> MyVar1 
+                = new StoreAnything<string, int, char> {var1 = "CS", var2 = 131, var3 = '!'};
+
+            Console.WriteLine($"{MyVar1.var1}{MyVar1.var2}{MyVar1.var3}"); //print new values of var1 and var2
+        }
+    }
+}
