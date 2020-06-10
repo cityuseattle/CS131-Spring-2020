d58f2eefbc4218b3f5dae86bdb2d090e25d2fb73
David Yi <yidavid@cityuniversity.edu>
2020-06-09 18:44:56 -0700

    Yi_PE10

---
+++ ON/Module10/PE10/Yi_ColorList/Yi_ColorList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace Yi_ColorList
+{
+    class Program
+    {
+        private static void ColorPrint(string s) //Creates a method called to print The colors in the list
+        {
+            Console.WriteLine(s);
+        }
+        
+        static void Main(string[] args)
+        {
+            List<string> ColorsList = new List<string>();//Creates the list
+            ColorsList.Add("White");
+            ColorsList.Add("Red");
+            ColorsList.Add("Orange");
+            ColorsList.Add("Yellow");
+            ColorsList.Add("Green");
+            ColorsList.Add("Blue");
+            ColorsList.Add("Indigo");
+            ColorsList.Add("Violet");
+            ColorsList.Add("Black");
+
+            Console.WriteLine("The color list is:");//First iteration of printing the list
+            foreach (string color in ColorsList)
+            {
+                ColorPrint(color);
+            }
+            Console.WriteLine();
+            
+            //Locates, removes and changes the list based on the index of the color Violet
+            int index = ColorsList.IndexOf("Violet");
+            ColorsList.RemoveAt(index);
+            ColorsList.Insert(index,"Purple");
+            
+            Console.WriteLine("The new color list is:");//Second iteration of printing the list
+            foreach (string color in ColorsList)
+            {
+                ColorPrint(color);
+            }
+            Console.WriteLine($"with the color Violet replaced with Purple" +
+                              $" at position {index+1} in the list.");
+            Console.WriteLine();
+
+            //Check for Magenta
+            if (ColorsList.Contains("Magenta"))
+            {
+                Console.WriteLine("Magenta is in the list.");
+            }
+            else ColorsList.Add("Magenta");//Adds magenta into the color list
+            
+            if (ColorsList.Contains("Magenta"))
+            {
+                int idx = ColorsList.IndexOf("Magenta");
+                Console.WriteLine($"Magenta is in the list at position: {idx + 1}.");
+            }
+            foreach (string color in ColorsList)//Last iteration of printing the list
+            {
+                ColorPrint(color);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ON/Module10/PE10/Yi_Swap/Yi_Swap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace Yi_Swap
+{
+    class Program
+    {
+        static void swap<T>(ref T a, ref T b)//Creates the swap method
+        {
+            T temp = a;
+            a = b;
+            b = temp; 
+        }
+        static void Main(string[] args)
+        {
+            //Establishing Variables
+            int a = 8241;
+            int b = 9534;
+            string i = "cat";
+            string j = "dog";
+            
+            //Body of code that calls swap of integers
+            Console.WriteLine($"Integer a before swap: {a}");
+            Console.WriteLine($"Integer b before swap: {b}");
+            swap(ref a, ref b); //swapping a and b
+            Console.WriteLine($"Integer a after swap: {a}");
+            Console.WriteLine($"Integer b after swap: {b}\n");
+            
+            //Body of code that calls swap of strings
+            Console.WriteLine($"String i before swap: {i}");
+            Console.WriteLine($"Integer j before swap: {j}");
+            swap(ref i, ref j); //swapping i and j
+            Console.WriteLine($"Integer i after swap: {i}");
+            Console.WriteLine($"Integer j after swap: {j}");
+        }
+    }
+}
