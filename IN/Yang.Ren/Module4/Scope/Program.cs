a0885f1fc467d8394e8a1fc7c90c3d71159b9220
YangRen <Yren550@gmail.com>
2020-06-10 05:26:20 -0700

    week4

---
+++ IN/Yang.Ren/Module4/Scope/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Scope
+{
+    class Program
+    {
+        public static string Str2;
+        static void ReturnString(string Str1)
+        {
+            string Str2 = "Programing in C#";
+
+            Console.Write(Str1);
+            
+        }
+        static void Main(string[] args)
+        {
+            ReturnString("CS131 ");
+            Console.WriteLine(Str2);
+        }
+    }
+}
