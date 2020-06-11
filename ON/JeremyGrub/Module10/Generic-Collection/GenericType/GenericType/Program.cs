7525ef6e35ec045c8625c7b60a5dc8451d431ee0
Jeremy <jkgrub@gmail.com>
2020-06-10 17:15:55 -0700

    Module 10

---
+++ ON/JeremyGrub/Module10/Generic-Collection/GenericType/GenericType/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace GenericType
+{
+    class Program
+    {
+        class StoreAnything<T, U, V> //declare class StoreAnything using generic trype
+        {
+            public T var1 {set; get; }
+            public U var2 { set; get; }
+            public V var3 { set; get; }
+        }
+        static void Main(string[] args)
+        {
+            //create new object of StoreAnything class, passing string and int type
+            StoreAnything<string, int, string> MyVar1 = new StoreAnything<string, int, string> { var1 = "CS", var2 = 131, var3 = "!" };
+
+            Console.WriteLine(MyVar1.var1 + MyVar1.var2 + MyVar1.var3); //print new values of var1 and var2
+        }
+    }
+}
---
+++ ON/JeremyGrub/Module10/Generic-Collection/HashTable/HashTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable myHashtbale = new Hashtable() //initialize a hash tbale using build in library Collections
+            {
+                {"Kim", 26}, //key = Kim; value = 26
+                {"John", 77}, //key = John; value = 77
+                {"Max", 50} //key = Max; value = 50
+            };
+
+            int HashTableVal1 = (int)myHashtbale["Kim"]; //get value of Kim and store it in HashTableVal1
+            int HashTableVal2 = (int)myHashtbale["John"]; //get value of John and store it in HashtTableVal2
+            int HashTableVal3 = (int)myHashtbale["Max"]; // get value of Max and store it in HashTableVal3
+            
+            //print on screen
+            Console.WriteLine(HashTableVal1);
+            Console.WriteLine(HashTableVal2);
+            Console.WriteLine(HashTableVal3);
+        }
+    }
+}
