94f8ce42f8b9a28e8eee7f31ab20be497da16d61
TrevorCanCode <63311674+TrevorCanCode@users.noreply.github.com>
2020-06-14 09:07:38 -0400

    Add files via upload

---
+++ ON/Module 10/Generic Collection/ArrayList/ArrayList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace Arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create Array list using built in library
+            ArrayList myArrList = new ArrayList();
+
+            //Add values to Array List using Add() method
+            myArrList.Add(10);
+            myArrList.Add(11.5);
+            myArrList.Add("This is a string");
+
+            //add values to array list using AddRange() method
+            myArrList.AddRange(new ArrayList() { 20, 17.7, "this is another string" });
+
+            //remove array list elements using RemoveRange() and RemoveAt() and remove () methods
+            myArrList.Remove(10);
+
+            myArrList.RemoveAt(0);
+
+            myArrList.RemoveRange(1, 2);
+
+            //print each value in the array list using foreach() method
+            foreach(var value in myArrList)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
---
+++ ON/Module 10/Generic Collection/Generic-Type/GenericType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Generic_Type
+{
+    class Program
+    {
+        class StoreAnything <T, U, I> //declare class StoreAnything using generic type
+        {
+            public T var1 { get; set; }
+            public U var2 { get; set; }
+            public I var3 { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            //create new object of StoreAnything class, passing string and int type and char type
+            StoreAnything<string, int, char> MyVar1 = new StoreAnything<string, int, char> { var1 = "CS", var2 = 131, var3 = '!'};
+
+            Console.WriteLine(MyVar1.var1 + " " + MyVar1.var2 + "" + MyVar1.var3); //print new values of var1 and var2
+            
+        }
+    }
+}
