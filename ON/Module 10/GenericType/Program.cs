7b3fce2eb4c996d48606c364c9fb5db985aff415
Marshall Townsend <Marshall@cityuniversity.edu>
2020-06-13 17:27:22 -0700

    Kim Week 10 updates

--- ON/Module 10/GenericType/Program.cs
+++ ON/Module 10/GenericType/Program.cs
@@ -4,9 +4,24 @@ namespace GenericType
 {
     class Program
     {
+        class StoreInt
+        {
+            public int num1 { set; get; }
+            public int num2 { set; get; }
+        }
+        class StoreString
+        { 
+            public string strg1 { set; get; }
+            public string strg2 { set; get; }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                StoreInt MyInt = new StoreInt { num1 = 1, num2 = 3 };
+                StoreString MyStrg = new StoreString { strg1 = "CS", strg2 = "131" };
+
+                Console.WriteLine(MyInt.num1 + "," + MyInt.num2);
+                Console.WriteLine(MyStrg.strg1 + "" + MyStrg.strg2);
+
         }
     }
 }
---
+++ ON/Module 10/GenericType1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GenericType1
+{
+    class Program
+    {
+        internal class StoreAnything<T1, T2, T3>
+        {
+            public string var1 { get; set; }
+            public int var2 { get; set; }
+            public object var3 { get; set; }
+
+        
+
+        }
+        static void Main(string[] args)
+        {
+            StoreAnything<string, int, object> MyVar1 = new StoreAnything<string, int, object> {var1 = "CS", var2 = 131, var3 = !};
+
+
+            Console.WriteLine(MyVar1.var1 + " " + MyVar1.var2 + MyVar1.var3); 
+        }
+
+    }
+
+    
+}
